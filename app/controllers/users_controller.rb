bc1511228a40d2f949f694eec7d390a4597b9058
Ralph stinson <raff.ross92@gmail.com>
2015-06-16 15:08:00 +0000

    Modified image size

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = user.includes(:profile)
+    @users = User.all
   end
   def show
     @user = User.find( params[:id] )
